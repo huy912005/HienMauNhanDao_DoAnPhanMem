d2fc5d18a435c4de0bd00d932fbaca9accfb9c83
HuyPham <phamminhhuy0901tk@gmail.com>
2026-04-26 00:00:22 +0700

    add backend java

---
+++ XayDungChuongTrinh/PhanMem/BackEnd/DoAnPhamMem/DoAnPhamMem/src/main/java/com/Nhom20/DoAnPhamMem/DoAnPhamMemApplication.java
@@ -0,0 +1,13 @@
+package com.Nhom20.DoAnPhamMem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnPhamMemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnPhamMemApplication.class, args);
+	}
+
+}
---
+++ XayDungChuongTrinh/PhanMem/BackEnd/DoAnPhamMem/DoAnPhamMem/src/main/java/com/Nhom20/DoAnPhamMem/repository/DonDangKyRepository.java
@@ -0,0 +1,4 @@
+package com.Nhom20.DoAnPhamMem.repository;
+
+public interface DonDangKyRepository {
+}
